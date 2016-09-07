422aed63bb68bddb178367bc4a9fa7eb20c74de4
Patrick <patrick.tran06@gmail.com>
2016-09-06 23:46:45 -0700

    Delete consoles

--- tester.js
+++ tester.js
@@ -1,10 +1,6 @@
 module.exports = function(arr) {
   var allPasses = true;
   return arr.reduce(function(acc, test) {
-    console.log('=== acc ===');
-    console.log(acc);
-    console.log('=== test ===');
-    console.log(test);
     if (!acc) return false;
     if (!test) return false;
     return acc;
